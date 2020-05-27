4d6836a945e953530667233db624d5ee4e6b5cad
zouxiang <xiang.zou@nokia-sbell.com>
2020-05-28 00:55:03 +0800

    add cases

--- src/main/java/medium/LengthOfLongestSubstring.java
+++ src/main/java/medium/LengthOfLongestSubstring.java
@@ -28,22 +28,28 @@ Note that the answer must be a substring, "pwke" is a subsequence and not a subs
 public class LengthOfLongestSubstring {
     public int lengthOfLongestSubstring(String s) {
         int result = 0;
+        if(s == null || "".equals(s)) return 0;
         char[] charArr = s.toCharArray();
         int startIdx = 0 , endIdx = 0;
         Map<Character, Integer> charMap = new HashMap();
         for(int i=0; i< charArr.length; i++) {
-            if(charMap.containsKey(charArr[i])) {
-                startIdx++;
-                charMap.put(charArr[i], i);
+            endIdx = i;
+            if(charMap.containsKey(charArr[i]) && charMap.get(charArr[i]) >= startIdx) {
+                startIdx = charMap.get(charArr[i])+1;
             } else {
-                charMap.put(charArr[i], i);
-                endIdx = i;
-                result++;
+                if(endIdx - startIdx > result) result = endIdx - startIdx;
             }
+            charMap.put(charArr[i], i);
         }
-        return result;
+        return result+1;
     }
     public static void main(String[] args) {
+        LengthOfLongestSubstring lengthOfLongestSubstring = new LengthOfLongestSubstring();
+        System.out.println(lengthOfLongestSubstring.lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(lengthOfLongestSubstring.lengthOfLongestSubstring("bbbbb"));
+        System.out.println(lengthOfLongestSubstring.lengthOfLongestSubstring("pwwkew"));
+        System.out.println(lengthOfLongestSubstring.lengthOfLongestSubstring("f;lajsdlfhkasf"));
+        System.out.println(lengthOfLongestSubstring.lengthOfLongestSubstring(""));
 
     }
 }
